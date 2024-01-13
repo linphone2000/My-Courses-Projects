735e313a3e08768f7ddd335333fc6c5243c1116d
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-01-13 20:47:32 +0630

    13/1/2024 8:47 PM

--- React/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,18 +1,4 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelcetSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
+export default function GameBoard({ onSelcetSquare, board }) {
 
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -30,12 +16,15 @@ export default function GameBoard({ onSelcetSquare, turns }) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelcetSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelcetSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
