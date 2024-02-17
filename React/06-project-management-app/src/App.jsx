cfbecf6740827cb6c420d4901adb24e0e1b21761
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-02-17 11:00:31 +0630

    17/2/24 11:00 AM

--- React/06-project-management-app/src/App.jsx
+++ React/06-project-management-app/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
