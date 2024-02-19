1bfcbf450b181f0a5f3fbfdb24764e8e7c0a9f34
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-02-19 18:53:46 +0630

    19/2/23 6:53 PM from laptop

--- React/06-project-management-app/src/App.jsx
+++ React/06-project-management-app/src/App.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject";
 import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
-      <p>Hi</p>
+      <ProjectsSidebar onAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
