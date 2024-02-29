5d10bb4a425072eb08168f9345f36e93eb563f39
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-02-29 08:38:46 +0630

    Start 09

---
+++ React/09-practice-useEffect/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoImg from "../assets/quiz-logo.png";
+
+export default function Quiz() {
+  return (
+    <header>
+      <img src={logoImg} alt="Quiz Logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
