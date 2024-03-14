ef99cfaab9ca28f7593a60fac15a7c762e5ec342
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-03-14 20:11:56 +0630

    14/3/24 8:12 PM

--- React/08-use-effect/src/components/Modal.jsx
+++ React/08-use-effect/src/components/Modal.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { useRef } from "react";
-import { createPortal } from "react-dom";
-
-const Modal = function Modal({ open,children }) {
-  const dialog = useRef();
-
-  
-
-  return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
-      {children}
-=======
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
@@ -28,7 +15,6 @@ const Modal = function Modal({ open, children, onClose }) {
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={onClose}>
       {open ? children : null}
->>>>>>> 0c531f5f5ef5a25727ffdd1d60d99e926a96c4b7
     </dialog>,
     document.getElementById("modal")
   );
